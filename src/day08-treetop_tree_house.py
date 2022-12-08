0bcee02f89ac0755e6fabc6ae3d0404ee55ba690
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2022-12-08 07:31:36 +0100

    Day 8: Treetop Tree House

---
+++ src/day08-treetop_tree_house.py
@@ -0,0 +1,45 @@
+from santas_little_helpers import day, get_data, timed, submit_answer
+from santas_little_utils import build_dict_map, directions_4, mul
+from itertools import product
+
+today = day(2022, 8)
+forest = None
+
+def visibility(comparison, x, y, xd, yd, depth=False):
+  pd = x-xd, y-yd
+  if pd not in forest:
+    return 0 if depth else True
+  if comparison <= forest[pd]:
+    return 1 if depth else False
+  inner = visibility(comparison, *pd, xd, yd, depth)
+  return (inner + 1) if depth else inner
+
+
+def is_visible(p):
+  tree_height = forest[p]
+  return any(visibility(tree_height, *p, *d) for d in directions_4)
+
+
+def trees_visible():
+  return sum(is_visible(p) for p in forest)
+
+
+def scenic_score(p):
+  tree_height = forest[p]
+  return mul(visibility(tree_height, *p, *d, depth=True) for d in directions_4)
+
+
+def most_scenic_tree():
+  w, h = max(x for x, _ in forest), max(y for _, y in forest)
+  return max(scenic_score(p) for p in product(range(1, h), range(1, w)))
+
+
+def main():
+  global forest
+  forest = build_dict_map(get_data(today))
+  print(f'{today} star 1 = {trees_visible()}')
+  print(f'{today} star 2 = {most_scenic_tree()}')
+
+
+if __name__ == '__main__':
+  timed(main)
