71dad23c677e533038bb13309ecfff842f5d4855
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2022-12-25 09:12:13 +0100

    Updated submission helper to support last day

--- src/santas_little_submission_helper.py
+++ src/santas_little_submission_helper.py
@@ -59,6 +59,9 @@ def __handle_response__(today: date, answer, level, submission_history, text):
     response['success'] = True
     emoji = '⭐️' if level == 1 else '🌟'
     print(f'Correct answer! {emoji}')
+  elif 'finished every puzzle' in text:
+    response['success'] = True
+    print("🎄 🌟 Advent of Code done, great job! 🌟 🎄")
   else:
     print(text)
 
@@ -86,6 +89,9 @@ def submit_answer(today: date, answer, level: int = 1, force = False) -> None:
     if answer in ['', 0]:
       print('Ignoring empty answer')
       return False
+  if level == 2 and not is_solved(get_submission_history(today, 1)):
+    print('Part 1 not solved, refusing to submit part 2')
+    return False
   submission_history = get_submission_history(today, level)
   if is_solved(submission_history):
     print(f'Already solved {today} part {level}')
@@ -115,36 +121,44 @@ def submit_answer(today: date, answer, level: int = 1, force = False) -> None:
     return False
 
 
-def redirect_stdout():
-  stream = sys.stdout = StringIO()
-  return stream
-
-
 def restore_stdout():
   sys.stdout = sys.__stdout__
 
 
-def __submit__(day):
-  stream = redirect_stdout()
-  theday = day.today
-  day.main()
-  restore_stdout()
+def __submit_output__(theday, output):
   answers = [None, None]
-  for line in stream.getvalue().strip().split('\n'):
+  for line in output.strip().split('\n')[-2:]:
     match = standard_answer_re.match(line)
     if match is None:
       print('Day not formatted using standard format')
       print(line)
       continue
     found_date, level, answer = match.groups()
-    assert(found_date == str(theday))
+    assert found_date == str(theday)
     answers[int(level)-1] = answer
-  assert(all(answer is not None for answer in answers))
+  if None in answers:
+    print('Missing answers, skipping submit')
+    return
   for idx in range(2):
     level = idx + 1
-    print(f'{theday} star {level}, submitting "{answers[idx]}"')
+    answer = answers[idx]
+    print(f'{theday} star {level}, submitting "{answer}"')
     submit_answer(theday, answer, level)
-    sleep(6) # there's flood protection between levels
+    if level == 1:
+      sleep(6) # there's flood protection between levels
+
+
+def submit_answers(theday, func):
+  with StringIO() as stream:
+    sys.stdout = stream
+    try:
+      func()
+      restore_stdout()
+      __submit_output__(theday, stream.getvalue())
+    except Exception as e:
+      restore_stdout()
+      print(f'Day {theday} failed to execute')
+      return
 
 
 def __submit_all__():
@@ -153,16 +167,18 @@ def __submit_all__():
     sleep(1)
   print()
   for file in sorted(Path('.').glob('day[!X]?-*.py')):
-    if 'day25' in file.name:
-      exit()
     try:
       day = importlib.import_module(file.name[:-3])
     except Exception as e:
       print(f'Failed to import \'{file.name}\': {e}', file=sys.stderr)
       print()
       continue
+    all_solved = all(is_solved(get_submission_history(day.today, idx + 1)) for idx in range(2))
+    if all_solved:
+      print(f'Already solved {day.today} skipping')
+      continue
     print(f'Running \'{file.name}\':')
-    __submit__(day)
+    submit_answers(day.today, day.main)
     print()
 
 
